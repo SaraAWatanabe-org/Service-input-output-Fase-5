c9a2e5a753faab43e44a6c48e6a6fedab943d6d3
Rafael Bernardo Roseno da Silva <rafaelroseno@outlook.com>
2025-01-12 13:47:53 -0300

    Initial commit

---
+++ src/main/java/com/example/service_input_output/ServiceInputOutputApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_input_output;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInputOutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceInputOutputApplication.class, args);
+	}
+
+}
