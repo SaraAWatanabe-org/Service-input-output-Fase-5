04826b6460cc0d9cc074d2b29bad5ea9ce137205
Marcelo C <marcelo.c.b22@gmail.com>
2025-01-19 13:40:22 -0300

    Ajustes @Values

--- src/main/java/com/challenge/fastfood/service_io/services/impl/S3ServiceImpl.java
+++ src/main/java/com/challenge/fastfood/service_io/services/impl/S3ServiceImpl.java
@@ -11,28 +11,28 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.challenge.fastfood.service_io.services.S3Service;
 
-
 @Service
 public class S3ServiceImpl implements S3Service {
 
-    private final AmazonS3 s3Client;
-    private final String bucketName;
-    
-    private final String folderName = "public/videos";
+	private final AmazonS3 s3Client;
+	
+	@Value("${aws.s3.bucketName}") 
+	private String bucketName;
 
-    public S3ServiceImpl(AmazonS3 s3Client, @Value("${aws.s3.bucketName}") String bucketName) {
-        this.s3Client = s3Client;
-        this.bucketName = bucketName;
-    }
+	private final String folderName = "public/videos";
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = folderName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+	public S3ServiceImpl(AmazonS3 s3Client) {
+		this.s3Client = s3Client;
+	}
 
-        s3Client.putObject(bucketName, fileName, file.getInputStream(), metadata);
-        return s3Client.getUrl(bucketName, fileName).toString();
-    }
+	public String uploadFile(MultipartFile file) throws IOException {
+		String fileName = folderName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(file.getSize());
+		metadata.setContentType(file.getContentType());
+
+		s3Client.putObject(bucketName, fileName, file.getInputStream(), metadata);
+		return s3Client.getUrl(bucketName, fileName).toString();
+	}
 
 }
